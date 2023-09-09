ffa6bd429b841eded8d68c5fc9326d85ee77e593
Naman1809 <72972766+Naman1809@users.noreply.github.com>
2023-09-09 09:24:04 +0530

    Add files via upload

---
+++ frontend/store.js
@@ -0,0 +1,14 @@
+import {configureStore} from "@reduxjs/toolkit";
+import { allUsersReducer, postOfFollowingReducer, userReducer } from "./Reducers/User";
+import { likeReducer, myPostsReducer } from "./Reducers/Post";
+ const store=configureStore({
+    reducer:{
+        user:userReducer,
+        postOfFollowing: postOfFollowingReducer,
+        allUsers:allUsersReducer,
+        like:likeReducer,
+        myPosts: myPostsReducer,
+    }
+});
+
+export default store;
